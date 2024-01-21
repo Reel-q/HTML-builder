9965fc6812bd8050a456d6ff8473c2eb52aea100
Reel <ladias231299@gmail.com>
2024-01-21 12:41:07 +0200

    feat: add solution for 3 task

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,21 @@
+// const { stdin, stdout } = process;
+const fs = require('fs');
+const path = require('path');
+
+const folderPath = path.join(__dirname, 'secret-folder');
+
+fs.readdir(folderPath, (error, files) => {
+  console.log(files);
+  files.forEach((file) => {
+    const filePath = path.join(folderPath, file);
+
+    fs.stat(filePath, (error, info) => {
+      if (info.isFile()) {
+        const fileName = path.parse(file).name;
+        const fileExtention = path.parse(file).ext.replace('.', '');
+        const fileSize = `${(info.size / 1024).toFixed(3)}kb`;
+        console.log(`${fileName} - ${fileExtention} - ${fileSize}`);
+      }
+    });
+  });
+});
