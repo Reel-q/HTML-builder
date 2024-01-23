66b73dc9328729fc12390d414ef79e3fbe9ef859
Reel <ladias231299@gmail.com>
2024-01-23 19:55:13 +0200

    refactor: 3 task

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,19 +1,23 @@
-const fs = require('fs');
+const fsPromises = require('fs').promises;
 const path = require('path');
 
 const folderPath = path.join(__dirname, 'secret-folder');
 
-fs.readdir(folderPath, (error, files) => {
-  files.forEach((file) => {
+async function logFiles() {
+  const folderItems = await fsPromises.readdir(folderPath);
+
+  for (const file of folderItems) {
     const filePath = path.join(folderPath, file);
 
-    fs.stat(filePath, (error, info) => {
-      if (info.isFile()) {
-        const fileName = path.parse(file).name;
-        const fileExtention = path.parse(file).ext.replace('.', '');
-        const fileSize = `${(info.size / 1024).toFixed(3)}kb`;
-        console.log(`${fileName} - ${fileExtention} - ${fileSize}`);
-      }
-    });
-  });
-});
+    const info = await fsPromises.stat(filePath);
+
+    if (info.isFile()) {
+      const fileName = path.parse(file).name;
+      const fileExtention = path.parse(file).ext.replace('.', '');
+      const fileSize = `${(info.size / 1024).toFixed(3)}kb`;
+      console.log(`${fileName} - ${fileExtention} - ${fileSize}`);
+    }
+  }
+}
+
+logFiles();
